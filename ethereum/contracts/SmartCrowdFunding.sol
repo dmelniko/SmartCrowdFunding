8564c718d9847cced51d1ddd015ceaddae80b78d
jzfraser <49706249+jzfraser@users.noreply.github.com>
2019-04-25 18:39:02 -0600

    Update SmartCrowdFunding.sol

--- ethereum/contracts/SmartCrowdFunding.sol
+++ ethereum/contracts/SmartCrowdFunding.sol
@@ -1,6 +1,35 @@
-pragma solidity ^0.4.25;
+pragma solidity ^0.5.2;
 
-contract SmartCrowdFunding {
+import './CrowdCoin.sol';
 
+import 'github.com/OpenZeppelin/openzeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
+import 'github.com/OpenZeppelin/openzeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol';
+import 'github.com/OpenZeppelin/openzeppelin-solidity/contracts/crowdsale/validation/TimedCrowdsale.sol';
+import 'github.com/OpenZeppelin/openzeppelin-solidity/contracts/crowdsale/distribution/PostDeliveryCrowdsale.sol';
 
+contract SmartCrowdFunding is Crowdsale, TimedCrowdsale, MintedCrowdsale, PostDeliveryCrowdsale {
+    constructor(
+        uint256 rate,   // in TKNbits, 400 for 
+        address payable wallet,
+        CrowdCoin token,
+        uint256 openingTime,
+        uint256 closingTime
+    )
+    MintedCrowdsale()
+    TimedCrowdsale(openingTime, closingTime)
+    PostDeliveryCrowdsale()
+    Crowdsale(rate, wallet, token)
+    public
+    {}
+}
+
+contract SmartCrowdFundingDeployer {
+    constructor()
+    public
+    {
+        CrowdCoin token = new CrowdCoin("CrowdCoin","CRC", 18);
+        Crowdsale SCF = new SmartCrowdFunding(100, msg.sender, token, now, now + 2629743);
+        token.addMinter(address(SCF));
+        token.renounceMinter();
+    }
 }
